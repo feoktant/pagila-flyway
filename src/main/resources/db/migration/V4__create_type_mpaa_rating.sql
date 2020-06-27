1e0250be4ba4417890d7fc1a94dbe70f3b2643f6
Anton Feoktistov <afeoktistoc@newagesol.co>
2020-06-27 13:32:42 +0300

    Added film table

---
+++ src/main/resources/db/migration/V4__create_type_mpaa_rating.sql
@@ -0,0 +1,1 @@
+CREATE TYPE mpaa_rating AS ENUM ('G', 'PG', 'PG-13', 'R', 'NC-17');
---
+++ src/main/resources/db/migration/V5__create_table_film.sql
@@ -0,0 +1,41 @@
+CREATE TABLE film
+(
+    film_id              SERIAL PRIMARY KEY,
+    title                TEXT                                                             NOT NULL,
+    description          TEXT,
+    release_year         year,
+    language_id          SMALLINT REFERENCES language ON UPDATE CASCADE ON DELETE RESTRICT NOT NULL,
+    original_language_id SMALLINT REFERENCES language ON UPDATE CASCADE ON DELETE RESTRICT,
+    rental_duration      SMALLINT      DEFAULT 3                                          NOT NULL,
+    rental_rate          NUMERIC(4, 2) DEFAULT 4.99                                       NOT NULL,
+    length               SMALLINT,
+    replacement_cost     NUMERIC(5, 2) DEFAULT 19.99                                      NOT NULL,
+    rating               mpaa_rating   DEFAULT 'G',
+    last_update          TIMESTAMPTZ   DEFAULT now()                                      NOT NULL,
+    special_features     TEXT[], -- SET('Trailers','Commentaries','Deleted Scenes','Behind the Scenes')
+    fulltext             TSVECTOR                                                         NOT NULL
+);
+
+CREATE TRIGGER last_updated
+    BEFORE UPDATE
+    ON film
+    FOR EACH ROW
+EXECUTE PROCEDURE last_updated();
+
+CREATE TRIGGER film_fulltext_trigger
+    BEFORE INSERT OR UPDATE
+    ON film
+    FOR EACH ROW
+EXECUTE PROCEDURE tsvector_update_trigger('fulltext', 'pg_catalog.english', 'title', 'description');
+
+CREATE INDEX film_fulltext_idx
+    ON film (fulltext);
+
+CREATE INDEX idx_title
+    ON film (title);
+
+CREATE INDEX idx_fk_language_id
+    ON film (language_id);
+
+CREATE INDEX idx_fk_original_language_id
+    ON film (original_language_id);
