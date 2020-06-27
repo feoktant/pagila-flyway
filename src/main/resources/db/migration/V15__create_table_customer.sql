1b402fa90e3753154a91782f9394d5f538c483b2
Anton Feoktistov <afeoktistoc@newagesol.co>
2020-06-27 15:43:34 +0300

    Added customer table

---
+++ src/main/resources/db/migration/V15__create_table_customer.sql
@@ -0,0 +1,19 @@
+CREATE TABLE customer
+(
+    customer_id SERIAL PRIMARY KEY,
+    store_id    SMALLINT REFERENCES store ON UPDATE CASCADE ON DELETE RESTRICT  NOT NULL,
+    first_name  TEXT                                                            NOT NULL,
+    last_name   TEXT                                                            NOT NULL,
+    email       TEXT,
+    address_id  INTEGER REFERENCES address ON UPDATE CASCADE ON DELETE RESTRICT NOT NULL,
+    activebool  BOOLEAN     DEFAULT true                                        NOT NULL,
+    create_date DATE        DEFAULT now()                                       NOT NULL,
+    last_update TIMESTAMPTZ DEFAULT now(), -- TODO why it is nullable?
+    active      INTEGER
+);
+
+CREATE TRIGGER last_updated
+    BEFORE UPDATE
+    ON customer
+    FOR EACH ROW
+EXECUTE PROCEDURE last_updated();
