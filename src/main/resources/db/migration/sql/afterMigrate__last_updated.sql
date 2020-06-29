0aa9e734e5cdaf00ab226a71a7ea20c45cb20d4f
Anton Feoktistov <afeoktistoc@newagesol.co>
2020-06-29 19:34:48 +0300

    Made trigger creation idempotent

--- src/main/resources/db/migration/sql/afterMigrate__last_updated.sql
+++ src/main/resources/db/migration/sql/afterMigrate__last_updated.sql
@@ -14,16 +14,18 @@ $$
         t record;
     BEGIN
         FOR t IN
-            SELECT table_name
+            SELECT *
             FROM information_schema.columns
             WHERE column_name = 'last_update'
+              AND table_schema = '${flyway:defaultSchema}'
             LOOP
-                EXECUTE format(
-                        'CREATE TRIGGER last_updated
+                EXECUTE format( -- TODO is it possible to select only needed tables?
+                        'DROP TRIGGER IF EXISTS last_updated ON %1$s.%2$s;
+                         CREATE TRIGGER last_updated
                          BEFORE UPDATE
-                         ON %I
+                         ON %1$s.%2$s
                          FOR EACH ROW EXECUTE PROCEDURE last_updated();',
-                        t.table_name);
+                        t.table_schema, t.table_name);
             END LOOP;
     END;
 $$ LANGUAGE plpgsql;
