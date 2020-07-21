11a05815f10f488588da72ba0e7fb81029762951
Anton Feoktistov <anton.feoktistov@gmail.com>
2020-07-22 00:40:15 +0300

    Create only needed trigers

--- src/main/resources/db/migration/sql/afterMigrate__last_updated.sql
+++ src/main/resources/db/migration/sql/afterMigrate__last_updated.sql
@@ -4,14 +4,19 @@ $$
         t record;
     BEGIN
         FOR t IN
-            SELECT *
-            FROM information_schema.columns
-            WHERE column_name = 'last_update'
-              AND table_schema = '${flyway:defaultSchema}'
+            SELECT clm.table_schema, clm.table_name
+            FROM information_schema.columns clm
+                     LEFT JOIN information_schema.triggers trg
+                               ON trg.trigger_catalog = clm.table_catalog
+                                   AND trg.trigger_schema = clm.table_schema
+                                   AND trg.event_object_table = clm.table_name
+            WHERE clm.column_name = 'last_update'
+              AND clm.table_schema = '${flyway:defaultSchema}'
+              AND trg.trigger_name IS NULL
             LOOP
-                EXECUTE format( -- TODO is it possible to select only needed tables?
-                        'DROP TRIGGER IF EXISTS last_updated ON %1$s.%2$s;
-                         CREATE TRIGGER last_updated
+                RAISE INFO 'Creating trigger for %.%', t.table_schema, t.table_name;
+                EXECUTE format(
+                        'CREATE TRIGGER last_updated
                          BEFORE UPDATE
                          ON %1$s.%2$s
                          FOR EACH ROW EXECUTE PROCEDURE last_updated();',
